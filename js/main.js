4b39701e510d32ced1d1de779ad9c7a80acc7972
Eike <eikeathylastb@gmail.com>
2020-03-27 22:07:47 -0300

    up

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 
     const defaults = {
         baseURLG: 'https://thevirustracker.com/free-api?global=stats',
-        baseURLE: 'https://thevirustracker.com/free-api?countryTotal=BR',
+        baseURLBR: 'https://thevirustracker.com/free-api?countryTotal=BR',
+        baseURLUS: 'https://thevirustracker.com/free-api?countryTotal=US',
         eleMain: document.getElementById('info'),
 
         eleInfcG: '#infcG',
@@ -12,18 +13,41 @@
         eleKillG: '#killG',
 
         eleTcB: '#tcB',
+        eleCaB: '#caB',
         eleCsB: '#csB',
         eleCnB: '#cnB',
         eleKB: '#kB',
         eleKnB: '#knB',
         eleCB: '#cB',
+        eleNcB: '#ncB',
+
+
+        eleTcUs: '#tcUs',
+        eleCaUs: '#caUs',
+        eleCsUs: '#csUs',
+        eleCnUs: '#cnUs',
+        eleKUs: '#kUs',
+        eleKnUs: '#knUs',
+        eleCUs: '#cUs',
+        eleNcUs: '#ncUs',
+
+
+        eleTcMund: '#tcMund',
+        eleCaMund: '#caMund',
+        eleCsMund: '#csMund',
+        eleCnMund: '#cnMund',
+        eleKMund: '#kMund',
+        eleKnMund: '#knMund',
+        eleCMund: '#cMund',
+        eleNcMund: '#ncMund',
     }
 
     const settings = {}
 
     const main = (event) => {
-        var geral = conectDb()
-        var br = conectDbE()
+        const geral = conectDb()
+        const br = conectDbBR()
+        const us = conectDbUS()
 
 
         settings.eleInfcG.innerHTML = fN(geral.results[0].total_cases)
@@ -31,23 +55,35 @@
         settings.eleCurG.innerHTML = fN(geral.results[0].total_recovered)
 
 
-
-
         settings.eleTcB.innerHTML = fN(br.countrydata[0].total_cases)
+        settings.eleCaB.innerHTML = fN(br.countrydata[0].total_active_cases)
         settings.eleCsB.innerHTML = fN(br.countrydata[0].total_serious_cases)
         settings.eleCnB.innerHTML = fN(br.countrydata[0].total_new_cases_today)
         settings.eleKB.innerHTML = fN(br.countrydata[0].total_deaths)
         settings.eleKnB.innerHTML = fN(br.countrydata[0].total_new_deaths_today)
         settings.eleCB.innerHTML = fN(br.countrydata[0].total_recovered)
+        settings.eleNcB.innerHTML = fN(br.countrydata[0].total_unresolved)
 
+        settings.eleTcUs.innerHTML = fN(us.countrydata[0].total_cases)
+        settings.eleCaUs.innerHTML = fN(us.countrydata[0].total_active_cases)
+        settings.eleCsUs.innerHTML = fN(us.countrydata[0].total_serious_cases)
+        settings.eleCnUs.innerHTML = fN(us.countrydata[0].total_new_cases_today)
+        settings.eleKUs.innerHTML = fN(us.countrydata[0].total_deaths)
+        settings.eleKnUs.innerHTML = fN(us.countrydata[0].total_new_deaths_today)
+        settings.eleCUs.innerHTML = fN(us.countrydata[0].total_recovered)
+        settings.eleNcUs.innerHTML = fN(us.countrydata[0].total_unresolved)
 
+        settings.eleTcMund.innerHTML = fN(geral.results[0].total_cases)
+        settings.eleCaMund.innerHTML = fN(geral.results[0].total_active_cases)
+        settings.eleCsMund.innerHTML = fN(geral.results[0].total_serious_cases)
+        settings.eleCnMund.innerHTML = fN(geral.results[0].total_new_cases_today)
+        settings.eleKMund.innerHTML = fN(geral.results[0].total_deaths)
+        settings.eleKnMund.innerHTML = fN(geral.results[0].total_new_deaths_today)
+        settings.eleCMund.innerHTML = fN(geral.results[0].total_recovered)
+        settings.eleNcMund.innerHTML = fN(geral.results[0].total_unresolved)
 
-        viewG(geral.results[0].total_cases, geral.results[0].total_deaths, geral.results[0].total_recovered)
-        viewB(br.countrydata[0].total_cases, br.countrydata[0].total_deaths, br.countrydata[0].total_recovered)
-
-
-        console.log(geral)
-        console.log(br)
+        viewG(geral.results[0].total_active_cases, geral.results[0].total_deaths, geral.results[0].total_recovered)
+        viewB(br.countrydata[0].total_active_cases, br.countrydata[0].total_deaths, br.countrydata[0].total_recovered)
     }
 
     const fN = function(value) {
@@ -65,14 +101,21 @@
 
     const conectDb = function() {
         var Dados = new XMLHttpRequest()
-        Dados.open("GET", settings.urlG, false)
+        Dados.open("POST", settings.urlG, false)
         Dados.send()
         return JSON.parse(Dados.responseText)
     }
 
-    const conectDbE = function() {
+    const conectDbBR = function() {
         var Geral = new XMLHttpRequest()
-        Geral.open("GET", settings.urlE, false)
+        Geral.open("POST", settings.urlBR, false)
+        Geral.send()
+        return JSON.parse(Geral.responseText)
+    }
+
+    const conectDbUS = function() {
+        var Geral = new XMLHttpRequest()
+        Geral.open("POST", settings.urlUS, false)
         Geral.send()
         return JSON.parse(Geral.responseText)
     }
@@ -82,7 +125,7 @@
         var pieGlobal = new Chart(pieGb, {
             type: 'doughnut',
             data: {
-                labels: ["Infectados", "Mortos", "Curados"],
+                labels: ["Casos ativos", "Mortos", "Curados"],
                 datasets: [{
                     data: [i, k, c],
                     backgroundColor: ['#e74a3b', '#858796', '#1cc88a'],
@@ -114,7 +157,7 @@
         var pieBrasil = new Chart(pieBr, {
             type: 'doughnut',
             data: {
-                labels: ["Infectados", "Mortos", "Curados"],
+                labels: ["Casos ativos", "Mortos", "Curados"],
                 datasets: [{
                     data: [i, k, c],
                     backgroundColor: ['#e74a3b', '#858796', '#1cc88a'],
@@ -144,7 +187,8 @@
 
     const setUp = function() {
         settings.urlG = defaults.baseURLG
-        settings.urlE = defaults.baseURLE
+        settings.urlBR = defaults.baseURLBR
+        settings.urlUS = defaults.baseURLUS
 
         settings.eleInfcG = defaults.eleMain.querySelector(defaults.eleInfcG)
         settings.eleCurG = defaults.eleMain.querySelector(defaults.eleCurG)
@@ -152,11 +196,33 @@
 
 
         settings.eleTcB = defaults.eleMain.querySelector(defaults.eleTcB)
+        settings.eleCaB = defaults.eleMain.querySelector(defaults.eleCaB)
         settings.eleCsB = defaults.eleMain.querySelector(defaults.eleCsB)
         settings.eleCnB = defaults.eleMain.querySelector(defaults.eleCnB)
         settings.eleKB = defaults.eleMain.querySelector(defaults.eleKB)
         settings.eleKnB = defaults.eleMain.querySelector(defaults.eleKnB)
         settings.eleCB = defaults.eleMain.querySelector(defaults.eleCB)
+        settings.eleNcB = defaults.eleMain.querySelector(defaults.eleNcB)
+
+
+        settings.eleTcUs = defaults.eleMain.querySelector(defaults.eleTcUs)
+        settings.eleCaUs = defaults.eleMain.querySelector(defaults.eleCaUs)
+        settings.eleCsUs = defaults.eleMain.querySelector(defaults.eleCsUs)
+        settings.eleCnUs = defaults.eleMain.querySelector(defaults.eleCnUs)
+        settings.eleKUs = defaults.eleMain.querySelector(defaults.eleKUs)
+        settings.eleKnUs = defaults.eleMain.querySelector(defaults.eleKnUs)
+        settings.eleCUs = defaults.eleMain.querySelector(defaults.eleCUs)
+        settings.eleNcUs = defaults.eleMain.querySelector(defaults.eleNcUs)
+
+
+        settings.eleTcMund = defaults.eleMain.querySelector(defaults.eleTcMund)
+        settings.eleCaMund = defaults.eleMain.querySelector(defaults.eleCaMund)
+        settings.eleCsMund = defaults.eleMain.querySelector(defaults.eleCsMund)
+        settings.eleCnMund = defaults.eleMain.querySelector(defaults.eleCnMund)
+        settings.eleKMund = defaults.eleMain.querySelector(defaults.eleKMund)
+        settings.eleKnMund = defaults.eleMain.querySelector(defaults.eleKnMund)
+        settings.eleCMund = defaults.eleMain.querySelector(defaults.eleCMund)
+        settings.eleNcMund = defaults.eleMain.querySelector(defaults.eleNcMund)
     }
 
     const events = function() {
